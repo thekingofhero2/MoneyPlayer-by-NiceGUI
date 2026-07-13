47e4ab751f061be5cb9ab53b100a70ed3834eab9
thekingofhero <thekingofhero@live.com>
2026-07-13 15:20:32 +0800

    修正密码72字节长度的问题

--- src/core/security.py
+++ src/core/security.py
@@ -3,9 +3,8 @@ from typing import Any
 from jose import jwt
 from passlib.context import CryptContext
 from src.core.config import settings
-import hashlib
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 ALGORITHM = "HS256"
 
@@ -22,17 +21,12 @@ def create_access_token(subject: str | Any, expires_delta: timedelta = None) -> 
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def get_safe_pwd_bytes(password: str) -> bytes:
-    # 先sha256摘要，输出固定32字节，永远<72
-    return hashlib.sha256(password.encode("utf-8")).digest()
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Compares a plain text password with a hashed password to check for a match."""
-    safe_bytes = get_safe_pwd_bytes(plain_password)
-    return pwd_context.verify(safe_bytes, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password: str) -> str:
     """Computes the bcrypt hash of a plain text password."""
-    safe_bytes = get_safe_pwd_bytes(password)
-    return pwd_context.hash(safe_bytes)
+    return pwd_context.hash(password)
